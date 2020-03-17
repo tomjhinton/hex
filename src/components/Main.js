b837359f819e15b6ea1341d245b167884d3a87c5
Tom Hinton <tomjhinton@gmail.com>
2020-03-17 11:31:38 +0000

    refactor

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,11 +11,7 @@ function Box1(props) {
   // This reference will give us direct access to the mesh
   const mesh1 = useRef()
   // console.log(props)
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
 
-  // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => (mesh1.current.rotation.x = mesh1.current.rotation.y += 0.01))
 
   return (
@@ -23,15 +19,13 @@ function Box1(props) {
       {...props}
 
       ref={mesh1}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
       onClick={e =>{
-      // setHover(false)
+
 
       props.passState(mesh1.current.color)
     }}
       onUpdate={self => self.material.color=new THREE.Color(props.color)}
-      onPointerOver={e => setHover(true)}
-      onPointerOut={e => setHover(false)}>
+      >
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshStandardMaterial attach="material" color={props.color} />
     </mesh>
@@ -42,25 +36,19 @@ function Box2(props) {
   // This reference will give us direct access to the mesh
   const mesh2 = useRef()
 
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
 
-  // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => (mesh2.current.rotation.x = mesh2.current.rotation.y += 0.01))
 
   return (
     <mesh
       {...props}
       ref={mesh2}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
       onClick={e =>{
-      // setHover(false)
+
 
       props.passState(mesh2.current.color)
     }}
-      onPointerOver={e => setHover(true)}
-      onPointerOut={e => setHover(false)}>
+      >
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshStandardMaterial attach="material" color={props.color} />
     </mesh>
@@ -71,33 +59,19 @@ function Box3(props) {
   // This reference will give us direct access to the mesh
   const mesh3 = useRef()
   // console.log(props)
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
 
-  // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => (mesh3.current.rotation.x = mesh3.current.rotation.y += 0.01))
 
   return (
     <mesh
       {...props}
       ref={mesh3}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
       onClick={e =>{
-      // setHover(false)
+
       console.log(e)
       props.passState(mesh3.current.color)
     }}
-  //     onPointerDown={e =>{
-  //     // setHover(false)
-  //     console.log("down")
-  //     props.passState(mesh3.current.color)
-  //   }}
-  //   onPointerUp={e =>{
-  //   // setHover(false)
-  //   console.log('up')
-  //   props.passState(mesh3.current.color)
-  // }}
+  //
     >
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshStandardMaterial attach="material" color={props.color} />
@@ -142,7 +116,7 @@ class Main extends React.Component{
 
   passState(value){
 
-    console.log(value)
+    // console.log(value)
     const hex = document.getElementById('hex')
     console.log(hex.innerText)
     if(hex.innerText === value){
@@ -151,7 +125,7 @@ class Main extends React.Component{
     if(hex.innerText !== value){
       this.setState({colors: [randColor(),randColor(), randColor()], score: this.state.score-1  })
     }
-    console.log(this.state)
+    // console.log(this.state)
 
 
   }
